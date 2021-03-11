fa2a2a781413b3136792d42028f783661757e568
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-11 21:50:07 +0100

    Trabalho Com Maps

---
+++ Maps.dart
@@ -0,0 +1,13 @@
+//Maps - Listas
+//Forma de Array
+
+main(){
+  //List<String> nomes = ["Adilson", "Icla", "Yago"];
+  //Lista de dados que recuperamos por string
+  //Ao contrario de arrays que se pega por indice.
+
+  var maps = {
+    
+  }
+
+}
