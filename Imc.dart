e15a5ef7062bc80455f250fcc8aea9b126db5e8e
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-11 21:47:08 +0100

    Funcao com retorno

--- Imc.dart
+++ Imc.dart
@@ -8,34 +8,39 @@ calculoDeImc();
 
 calculoDeImc(){
 print("=== DIGITE SEU PESO ===");
-var textPeso = stdin.readLineSync();
-var peso = int.parse(textPeso);
+String textPeso = stdin.readLineSync();
+int peso = int.parse(textPeso);
 
 print("=== DIGITE SUA ALTURA ===");
-var textAltura = stdin.readLineSync();
-var altura = double.parse(textAltura);
+String textAltura = stdin.readLineSync();
+double altura = double.parse(textAltura);
 
-var calcImc = peso / (altura * altura);
-
-imprimirResultado(calcImc);
+double imc = calcImc(peso,altura);
+imprimirResultado(imc);
 
 }
 
-imprimirResultado(calcImc){
+//Funcao Com Retorno
+calcImc(int peso,double altura){
+return peso / (altura * altura);
+}
+
+//Funcao Simples Com Parametro
+imprimirResultado(double imc){
 print("===============================");
-if(calcImc < 18.5){
+if(imc < 18.5){
   print("Abaixo Do Peso");
   
-}else if(calcImc > 18.5 && calcImc < 24.9){
+}else if(imc > 18.5 && imc < 24.9){
   
   print("Peso Normal");
-}else if(calcImc > 25 && calcImc < 29.9){
+}else if(imc > 25 && imc < 29.9){
   
   print("Sobrepeso");
-}else if(calcImc > 30 && calcImc < 34.9){
+}else if(imc > 30 && imc < 34.9){
   
   print("Obesidade grau 1");
-}else if(calcImc >35 && calcImc < 39.9){
+}else if(imc >35 && imc < 39.9){
  
   print("Obesidade grau 2");
 }else{
