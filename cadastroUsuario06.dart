1b2da861056726e07d867cf02e6d0e69c5fc336a
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-11 23:00:45 +0100

    Criando keys dinamicamente(maps)

---
+++ cadastroUsuario06.dart
@@ -0,0 +1,14 @@
+//Usando Maps novamente
+import "dart:io";
+
+
+//Map ttipado(com variaveis especificas);
+Map<String, dynamic> cadastro;
+
+main(){
+
+  print("====Digite seu nome====")
+  String nome = stdin.readLineSync();
+  //Criar key em tempo de execucao
+  //cadastro
+}
