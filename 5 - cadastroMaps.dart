13f0335d8b2ac8fe6de8237681ee5db4b942b18a
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-11 22:25:59 +0100

    Correcao de bugs.

--- 5 - cadastroMaps.dart
+++ 5 - cadastroMaps.dart
@@ -1,25 +1,41 @@
 import "dart:io";
 
 main(){
+//Map
+Map<String, dynamic> cadastro = {};
 
-print("========Digite o seu nome========");
-String nome = stdin.readLineSynce();
-cadastro["nome"] = nome;
+bool condicao = true;
 
-print("========Digite sua idade========");
-String nome = stdin.readLineSynce();
-cadastro["nome"] = nome;
+print("===========DIGITE UM COMANDO==========")
+while(condicao){
+  String  comando = stdin.readLineSync();
+  if(comando == "sair");
+  print("Programa Finalizado");
+  condicao = false;
+} else if (comando == "cadastro"){
+  cadastrar();
+}else if(comando == "imprimir"){
+  print(cadastro);
+}else {
+  print("Comando Inexistente.");
+}
 
-print("========Digite o seu nome========");
-String nome = stdin.readLineSynce();
-cadastro["nome"] = nome;
 
-print("========Digite o seu nome========");
-String nome = stdin.readLineSynce();
-cadastro["nome"] = nome;
+}
 
-print("========Digite o seu nome========");
-String nome = stdin.readLineSynce();
-cadastro["nome"] = nome;
+cadastrarPessoas(){
+  print("========Digite o seu Nome========");
+cadastro["nome"] = stdin.readLineSynce();
 
+print("========Digite sua Idade========");
+
+cadastro["idade"] = stdin.readLineSynce();
+
+print("========Digite sua Cidade========");
+cadastro["cidade"] = stdin.readLineSynce();
+
+print("========Digite o sua Provincia========");
+cadastro["provincia"] = stdin.readLineSynce();
+
+print(cadastro);
 }
