8cad864e32e71a8ea973407ea9bd76e2badd1560
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-11 23:05:17 +0100

    Resolucao de bug

--- cadastroUsuario06.dart
+++ cadastroUsuario06.dart
@@ -3,12 +3,29 @@ import "dart:io";
 
 
 //Map ttipado(com variaveis especificas);
-Map<String, dynamic> cadastro;
+Map<String, dynamic> cadastro = ;
 
 main(){
 
-  print("====Digite seu nome====")
+  print("====Digite seu nome====");
   String nome = stdin.readLineSync();
   //Criar key em tempo de execucao
-  //cadastro
+  cadastro["nome"] = nome;
+
+   print("====Digite seu Idade====");
+  String idade = stdin.readLineSync();
+  //Criar key em tempo de execucao
+  cadastro["idade"] = idade;
+
+   print("====Digite seu Provincia====");
+  String provincia = stdin.readLineSync();
+  //Criar key em tempo de execucao
+  cadastro["provincia"] = provincia;
+
+   print("====Digite seu Cidade====");
+  String cidade = stdin.readLineSync();
+  //Criar key em tempo de execucao
+  cadastro["cidade"] = cidade;
+
+  print(cadastro);
 }
