52d24502f51a2b0f7b808797dfd29c90f7fbd79e
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-11 22:15:31 +0100

    Organizando os exercicios

---
+++ 5 - cadastroMaps.dart
@@ -0,0 +1,25 @@
+import "dart:io";
+
+main(){
+
+print("========Digite o seu nome========");
+String nome = stdin.readLineSynce();
+cadastro["nome"] = nome;
+
+print("========Digite sua idade========");
+String nome = stdin.readLineSynce();
+cadastro["nome"] = nome;
+
+print("========Digite o seu nome========");
+String nome = stdin.readLineSynce();
+cadastro["nome"] = nome;
+
+print("========Digite o seu nome========");
+String nome = stdin.readLineSynce();
+cadastro["nome"] = nome;
+
+print("========Digite o seu nome========");
+String nome = stdin.readLineSynce();
+cadastro["nome"] = nome;
+
+}
