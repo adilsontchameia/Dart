d4fb8e2cf7d851532a3f8f7992243d372bf47601
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-11 22:39:37 +0100

    Organizacao das classes

---
+++ exercicios01.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+main(){
+
+var num1 = 3;
+var num2 = 5;
+
+var isTrue = num1 >= num2;
+
+//isTrue
+if(isTrue){
+
+print("Verdadeiro");
+
+}else{
+
+print("Falso");
+
+
+}
+
+}
---
+++ maps03.dart
@@ -0,0 +1,26 @@
+//Maps - Listas
+//Forma de Array
+
+main(){
+  //List<String> nomes = ["Adilson", "Icla", "Yago"];
+  //Lista de dados que recuperamos por string
+  //Ao contrario de arrays que se pega por indice.
+
+  Map<> maps = {
+    "nome" : "Adilson Chameia",
+    "idade" : 19,
+    "provincia" : "Cuando Cubango",
+    "cidade" : "Menongue",
+};
+  //Ao inves de passar indice como array, apenas passa-se a key.
+
+  //Atribuir valor fora do map padrao criado
+    maps["nome"] = "Yago Chameia";
+    //Fim
+
+    print("Nome: " + maps["nome"]); 
+    print(maps["idade"]); 
+
+
+//Map = forma para representar dados, nao propriamente para uma lista.
+}
