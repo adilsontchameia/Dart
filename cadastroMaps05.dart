f6b8f12296ce9dea8b65a589b6282d0b7dbfcdb1
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-13 21:45:58 +0100

    Criacao de lista de maps

--- cadastroMaps05.dart
+++ cadastroMaps05.dart
@@ -1,47 +1,48 @@
 import "dart:io";
 
-//Map
-Map<String, dynamic> cadastro = {};
+import 'cadastroUsuario06.dart';
 
-main(){
+//Lista de map
+List<Map<String, dynamic>> cadastros = [];
 
-bool condicao = true;
+main() {
+  bool condicao = true;
 
- print("\x1B[2J\x1B[0;0H");
-while(condicao){
-  print("===========DIGITE UM COMANDO==========");
+  print("\x1B[2J\x1B[0;0H");
+  while (condicao) {
+    print("===========DIGITE UM COMANDO==========");
 
-  var  comando = stdin.readLineSync();
-  if(comando == "sair"){
-  print("Programa Finalizado");
+    var comando = stdin.readLineSync();
+    if (comando == "sair") {
+      print("Programa Finalizado");
 
-  condicao = false;
-} else if (comando == "cadastro"){
-
-  cadastrarPessoas();
-}else if(comando == "imprimir"){
-  print(cadastro);
-
-}else {
-  print("Comando Inexistente.");
+      condicao = false;
+    } else if (comando == "cadastro") {
+      cadastrar();
+    } else if (comando == "imprimir") {
+      print(cadastros);
+    } else {
+      print("Comando Inexistente.");
+    }
+  }
 }
 
-}
-}
+cadastrar() {
+  //Map
+  Map<String, dynamic> cadastro = {};
 
-cadastrarPessoas(){
   print("========Digite o seu Nome========");
-cadastro["nome"] = stdin.readLineSync();
+  cadastro["nome"] = stdin.readLineSync();
 
-print("========Digite sua Idade========");
+  print("========Digite sua Idade========");
 
-cadastro["idade"] = stdin.readLineSync();
+  cadastro["idade"] = stdin.readLineSync();
 
-print("========Digite sua Cidade========");
-cadastro["cidade"] = stdin.readLineSync();
+  print("========Digite sua Cidade========");
+  cadastro["cidade"] = stdin.readLineSync();
 
-print("========Digite o sua Provincia========");
-cadastro["provincia"] = stdin.readLineSync();
+  print("========Digite o sua Provincia========");
+  cadastro["provincia"] = stdin.readLineSync();
 
-print(cadastro);
+  cadastros.add(cadastro);
 }
